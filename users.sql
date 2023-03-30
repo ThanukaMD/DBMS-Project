c051b323c9d94dd358c24e2a034e3acadb9dda24
ThanukaMD <94976220+ThanukaMD@users.noreply.github.com>
2023-03-30 19:59:46 -0300

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,42 @@
+-- Create a database named 'fastnfresh'
+CREATE DATABASE fastnfresh;
+
+-- Switch to the newly created database
+USE fastnfresh;
+
+-- Create a table to store user information
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phonenumber VARCHAR(20) NOT NULL,
+    creditcard VARCHAR(20) NOT NULL,
+    usercreated DATE NOT NULL
+);
+
+-- Create a table to store shopping history
+CREATE TABLE shoppinghistory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    purchase_date DATETIME NOT NULL,
+    product_details TEXT NOT NULL,
+    final_total DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- Insert some dummy data into the users table
+INSERT INTO users (
+    firstname, lastname, username, email, password, address, phonenumber, creditcard, usercreated
+) VALUES (
+    'John', 'Doe', 'john_doe', 'john@example.com', PASSWORD('johnspassword'), '123 Main St', '1234567890', '1234567812345678', '2023-01-01'
+);
+
+INSERT INTO users (
+    firstname, lastname, username, email, password, address, phonenumber, creditcard, usercreated
+) VALUES (
+    'Jane', 'Smith', 'jane_smith', 'jane@example.com', PASSWORD('janespassword'), '456 Market St', '0987654321', '8765432187654321', '2023-02-01'
+);
